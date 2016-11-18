47a6e74b5d676b22ef2bff715df034be2987a5bf
Alexandre Hobeika <alexandre.hobeika@gmail.com>
2016-11-19 00:53:52 +0100

    Add iconv test function

--- test-rstudio-github.R
+++ test-rstudio-github.R
@@ -2,3 +2,9 @@
 
 print( "Bon, ça a l'air de marcher!" )
 
+# Tester tous les encodages de iconv() sur une chaîne de caractères
+all_iconv_encs <- function( string ){
+  sapply( iconvlist(), function(x){
+    iconv( x, from = x )
+  } )
+}
